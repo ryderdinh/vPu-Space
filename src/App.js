813c05d1b27b9685c99544289f06d9fa673dda4d
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2021-08-27 21:44:48 +0700

    update design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,245 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="trello-dinhquanganh">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorms</header>
+          <ul>
+            <li>
+              <img
+                src="https://miro.medium.com/max/1400/1*lmWVKjCKw6yqxKkAIKUMng.gif"
+                alt="dinhquanganh"
+              />
+              Title: dinhquanganh profile
+            </li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+          </ul>
+          <footer>Add task 😐</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorms</header>
+          <ul>
+            <li>
+              <img
+                src="https://miro.medium.com/max/1400/1*lmWVKjCKw6yqxKkAIKUMng.gif"
+                alt="dinhquanganh"
+              />
+              Title: dinhquanganh profile
+            </li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+          </ul>
+          <footer>Add task 😐</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorms</header>
+          <ul>
+            <li>
+              <img
+                src="https://miro.medium.com/max/1400/1*lmWVKjCKw6yqxKkAIKUMng.gif"
+                alt="dinhquanganh"
+              />
+              Title: dinhquanganh profile
+            </li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+          </ul>
+          <footer>Add task 😐</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorms</header>
+          <ul>
+            <li>
+              <img
+                src="https://miro.medium.com/max/1400/1*lmWVKjCKw6yqxKkAIKUMng.gif"
+                alt="dinhquanganh"
+              />
+              Title: dinhquanganh profile
+            </li>
+
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+          </ul>
+          <footer>Add task 😐</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorms</header>
+          <ul>
+            <li>
+              <img
+                src="https://miro.medium.com/max/1400/1*lmWVKjCKw6yqxKkAIKUMng.gif"
+                alt="dinhquanganh"
+              />
+              Title: dinhquanganh profile
+            </li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+          </ul>
+          <footer>Add task 😐</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorms</header>
+          <ul>
+            <li>
+              <img
+                src="https://miro.medium.com/max/1400/1*lmWVKjCKw6yqxKkAIKUMng.gif"
+                alt="dinhquanganh"
+              />
+              Title: dinhquanganh profile
+            </li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+          </ul>
+          <footer>Add task 😐</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorms</header>
+          <ul>
+            <li>
+              <img
+                src="https://miro.medium.com/max/1400/1*lmWVKjCKw6yqxKkAIKUMng.gif"
+                alt="dinhquanganh"
+              />
+              Title: dinhquanganh profile
+            </li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+          </ul>
+          <footer>Add task 😐</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorms</header>
+          <ul>
+            <li>
+              <img
+                src="https://miro.medium.com/max/1400/1*lmWVKjCKw6yqxKkAIKUMng.gif"
+                alt="dinhquanganh"
+              />
+              Title: dinhquanganh profile
+            </li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+          </ul>
+          <footer>Add task 😐</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorms</header>
+          <ul>
+            <li>
+              <img
+                src="https://miro.medium.com/max/1400/1*lmWVKjCKw6yqxKkAIKUMng.gif"
+                alt="dinhquanganh"
+              />
+              Title: dinhquanganh profile
+            </li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+            <li>Add new task to work on below</li>
+          </ul>
+          <footer>Add task 😐</footer>
+        </div>
+      </div>
     </div>
   );
 }
