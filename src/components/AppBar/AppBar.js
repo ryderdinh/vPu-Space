4e390b20112e7521ce39d1cea1247517eb85e532
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2021-08-29 21:03:18 +0700

    add_new_column-bootstrap

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -3,5 +3,5 @@ import "./AppBar.scss";
 import React from "react";
 
 export default function AppBar() {
-  return <nav className="navbar-app">vTrend on Pu</nav>;
+  return <nav className="navbar-app">vPu-Space</nav>;
 }
--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,15 +1,29 @@
 import "./BoardContent.scss";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { isEmpty } from "lodash";
 import { mapOrder } from "utilities/sorts";
 import Column from "components/Column/Column";
 import { initialData } from "actions/initialData";
 import { Container, Draggable } from "react-smooth-dnd";
 import { applyDrag } from "utilities/dragDrop";
+import {
+  Button,
+  Col,
+  Container as BootstrapContainer,
+  Form,
+  Row,
+} from "react-bootstrap";
 
 export default function BoardContent() {
+  //? States
   const [board, setBoard] = useState({});
   const [columns, setColumns] = useState([]);
+  const [openNewColumnForm, setOpenNewColumnForm] = useState(false);
+  const [newColumnTitle, setNewColumnTitle] = useState("");
+
+  //? Refs
+  const newColumnInputRef = useRef();
+
   const onColumnDrop = (dropResult) => {
     let newColumns = [...columns];
     newColumns = applyDrag(newColumns, dropResult);
@@ -29,6 +43,27 @@ export default function BoardContent() {
       setColumns(newColumns);
     }
   };
+  const addNewColumn = () => {
+    const newColumnData = {
+      id: Math.random().toString(36).substr(2, 7), //random characters, will remove when we implement code api
+      boardId: board.id,
+      title: newColumnTitle.trim(),
+      cardOrder: [],
+      cards: [],
+    };
+    let newColumns = [...columns];
+    newColumns.push(newColumnData);
+    setColumns(newColumns);
+
+    let newBoard = { ...board };
+    newBoard.columnOrder = newColumns.map((column) => column.id);
+    newBoard.columns = newColumns;
+    setBoard(newBoard);
+    setNewColumnTitle("");
+    setOpenNewColumnForm(false);
+  };
+
+  //? Effects
   useEffect(() => {
     const boardFromDb = initialData.boards.find(
       (board) => board.id === "brd-1"
@@ -39,6 +74,11 @@ export default function BoardContent() {
       setColumns(mapOrder(boardFromDb.columns, boardFromDb.columnOrder, "id"));
     }
   }, []);
+  useEffect(() => {
+    if (newColumnInputRef && newColumnInputRef.current) {
+      newColumnInputRef.current.focus();
+    }
+  }, [openNewColumnForm]);
 
   if (isEmpty(board)) {
     return <div className="not-found">Board not found</div>;
@@ -57,7 +97,7 @@ export default function BoardContent() {
         }}
         getChildPayload={(index) => columns[index]}
       >
-        {columns.map((column, index) => (
+        {columns.map((column) => (
           <Draggable key={column.id}>
             <Column
               title={column.title}
@@ -69,10 +109,50 @@ export default function BoardContent() {
           </Draggable>
         ))}
       </Container>
-      <div className="add-new-column">
-        <i className="fa fa-plus icon" />
-        Add new column
-      </div>
+      <BootstrapContainer className="bootstrap-container">
+        {!openNewColumnForm ? (
+          <Row>
+            <Col
+              className="add-new-column"
+              onClick={() => {
+                setOpenNewColumnForm(!openNewColumnForm);
+              }}
+            >
+              <i className="fa fa-plus icon" />
+              Add new column
+            </Col>
+          </Row>
+        ) : (
+          <Row>
+            <Col className="enter-new-column">
+              <Form.Control
+                size="sm"
+                type="text"
+                value={newColumnTitle}
+                placeholder="Enter column title"
+                className="input-enter-new-column"
+                ref={newColumnInputRef}
+                onChange={(e) => setNewColumnTitle(e.target.value)}
+                onKeyPress={(e) => e.code === "Enter" && addNewColumn()}
+              />
+              <Button variant="success" size="sm" onClick={addNewColumn}>
+                Add column
+              </Button>
+              <Button
+                variant="danger"
+                size="sm"
+                className="cancel-new-column"
+                onClick={() => {
+                  setOpenNewColumnForm(!openNewColumnForm);
+                  setNewColumnTitle("");
+                }}
+              >
+                <i className="fa fa-trash icon" />
+              </Button>
+            </Col>
+          </Row>
+        )}
+      </BootstrapContainer>
     </div>
   );
 }
