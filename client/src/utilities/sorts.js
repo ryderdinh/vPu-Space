2f586fd83c5dc357407ecd07d3e0195908be3e70
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2021-08-31 21:46:00 +0700

    change-directory-structure/init-nodejs

---
+++ client/src/utilities/sorts.js
@@ -0,0 +1,5 @@
+//? Order an array of objects based on another array order
+export const mapOrder = (array, order, key) => {
+  array.sort((a, b) => order.indexOf(a[key]) - order.indexOf(b[key]))
+  return array
+}
--- src/utilities/sorts.js
+++ src/utilities/sorts.js
@@ -1,5 +1,7 @@
 //? Order an array of objects based on another array order
-export const mapOrder = (array, order, key) => {
-  array.sort((a, b) => order.indexOf(a[key]) - order.indexOf(b[key]))
-  return array
-}
+const mapOrder = (array, order, key) => {
+  array.sort((a, b) => order.indexOf(a[key]) - order.indexOf(b[key]));
+  return array;
+};
+
+export { mapOrder };
