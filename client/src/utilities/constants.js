2f916b3afb60be3e2a1a9e3ca02081ada8b31208
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2021-10-02 15:43:06 +0700

    deploy-site-2nd

--- client/src/utilities/constants.js
+++ client/src/utilities/constants.js
@@ -1,3 +1,4 @@
 export const MODAL_ACTION_CLOSE = 'MODAL_ACTION_CLOSE';
 export const MODAL_ACTION_CONFIRM = 'MODAL_ACTION_CONFIRM';
-export const API_ROOT = 'http://localhost:4700';
+// export const API_ROOT = 'http://localhost:4700';
+export const API_ROOT = 'https://vpuserver.herokuapp.com';
